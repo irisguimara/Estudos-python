2bd7bb69489701112760a7145708cacc7e73f759
irisguimara <irisguimaraes80@gmail.com>
2026-06-18 22:04:28 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print ("sistema de media")
+nota1=float(input("Primeira nota: "))
+nota2=float(input("Segunda nota: "))
+nota3=float(input("Terceira nota: "))
+media=((nota1 + nota2 + nota3)/3)
+print (media)
