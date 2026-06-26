5b22ebcab326f88f39a1bea1391940f0abbfcde9
irisguimara <irisguimaraes80@gmail.com>
2026-06-26 19:53:25 -0300

    Add files via upload

---
+++ desafio2.py
@@ -0,0 +1,9 @@
+print('=====🏍️  Verificador de CNH  🚗=====')
+
+idade =int(input ('Qual sua idade?'))
+
+if idade >=18:
+    print('Você já pode tirar sua CNH! 🏁')
+
+else:
+    print('Você ainda não pode tirar a CNh!❌')
